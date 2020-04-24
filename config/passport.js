ab5d943ecaadfcce4253c123bfa6110df77f37bd
ludennis-dlsu <60458147+ludennis-dlsu@users.noreply.github.com>
2020-04-24 21:14:36 +0800

    Initial Upload (4/24)

---
+++ config/passport.js
@@ -0,0 +1,40 @@
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+const UserAccount= require('../models/userAccountModel');
+
+module.exports = function(passport){
+    passport.use(
+        new LocalStrategy({usernameField: 'email', passwordField: 'password'}, (email, password, done) => {
+            UserAccount.findOne({ email: email})
+            .then( user => {
+                if(!user){
+                    return done(null, false, {message: "Email is not registered"});
+                }
+                
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if(err) throw err;
+
+                    if(isMatch){
+                        return done(null, user);
+                    }else{
+                        return done(null, false, {message: "Password incorrect"});
+                    }
+                })
+            })
+            .catch(err => console.log(err));
+        })
+    );
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+    
+    passport.deserializeUser(function(id, done) {
+        UserAccount.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+}
---
+++ models/userAccountModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const dbURL = 'mongodb+srv://user:pAssword@recipeep-nxraf.mongodb.net/recipeepdb';
+
+const options = { useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false };
+
+mongoose.connect(dbURL, options);
+
+const UserAccountSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profPic: {
+        type: String
+    },   
+    description: {
+        type: String
+    },
+    dateJoined: {
+        type: Date,
+        defualt: Date.now
+    },
+    recipePost: [{type: mongoose.Schema.Types.ObjectId, ref: 'post_full'}]
+});
+
+module.exports = mongoose.model('user_account', UserAccountSchema);
