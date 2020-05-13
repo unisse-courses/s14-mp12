538ce7f77b0090fb9e76f93548ec9b25ab2dc99e
rainecortel <lorraine_renee_cortel@dlsu.edu.ph>
2020-05-13 17:26:48 +0800

    526pm

--- public/scripts/post-full.js
+++ public/scripts/post-full.js
@@ -2,5 +2,23 @@ $(function() {
     $('.pop').on('click', function() {
         $('.image-preview').attr('src', $(this).find('img').attr('src'));
         $('#image-modal').modal('show');   
-    });		
+    });	
+    
+    $('#cancel-star').click(function() {
+        console.log('Cancel Rating');
+
+        $('input[type="radio"]').prop('checked', false);
+        $('#selected-rating').text('0');
+    });
+
+    $('.close').click(function() {
+        console.log('Cancel Rating');
+
+        $('input[type="radio"]').prop('checked', false);
+        $('#selected-rating').text('0');
+    });
+    
+    $('#rating-form').on('change','[name="pfRating"]',function(){
+        $('#selected-rating').text($('[name="pfRating"]:checked').val());
+    });
 });
