e575e41360b18a982bee6f32db6a29e1fb391a36
rainecortel <lorraine_renee_cortel@dlsu.edu.ph>
2020-05-13 19:20:19 +0800

    720pm

--- public/scripts/post-full.js
+++ public/scripts/post-full.js
@@ -8,6 +8,9 @@ $(function() {
         console.log('Cancel Rating');
 
         $('input[type="radio"]').prop('checked', false);
+        for (var y = 1; y <= 5; y++) {
+            $("#star" + y).css("color", "#777777");
+        }
         $('#selected-rating').text('0');
     });
 
@@ -15,10 +18,22 @@ $(function() {
         console.log('Cancel Rating');
 
         $('input[type="radio"]').prop('checked', false);
+        for (var y = 1; y <= 5; y++) {
+            $("#star" + y).css("color", "#777777");
+        }
         $('#selected-rating').text('0');
     });
     
     $('#rating-form').on('change','[name="pfRating"]',function(){
-        $('#selected-rating').text($('[name="pfRating"]:checked').val());
+        for (var y = 1; y <= 5; y++) {
+            $("#star" + y).css("color", "#777777");
+        }
+
+        var checkedVal = $('[name="pfRating"]:checked').val();
+
+        $('#selected-rating').text(checkedVal);
+        for (var x = 1; x <= checkedVal; x++) {
+            $("#star" + x).css("color", "#ffd100");
+        }
     });
 });
