da0cea7d1accf808c0b67713b67ca9f3151f3b8a
rainecortel <lorraine_renee_cortel@dlsu.edu.ph>
2020-05-13 16:16:51 +0800

    416pm

--- public/scripts/edit-comment.js
+++ public/scripts/edit-comment.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+    var currCommentDiv;
+    var currActionDiv;
+    var currEditDiv;
+
     $('#edit-comment').on('click', function(e) {
         console.log("Clicked Edit Comment");
         var mainDiv = $(this).parent().parent().parent().parent();
@@ -8,28 +12,27 @@ $(document).ready(function() {
         console.log(mainDivId);
 
         var commentDiv = $(mainDivId).find('.comment-details');
+        var editDiv = $(mainDivId).find('.edit-comment-details');
+        var actionDiv = $(mainDivId).find('.comment-actions');
+
+        currCommentDiv = commentDiv;
+        currActionDiv = actionDiv;
+        currEditDiv = editDiv;
+
         var commentContent = $(mainDivId).find('.comment-details').text();
         commentContent = commentContent.replace(/\s{2,}/g, ' ');
         var fixedComment = $.trim(commentContent);
-        console.log(commentDiv);
 
-        var actionDiv = $(mainDivId).find('.comment-actions');
-
-        commentDiv.empty();
+        commentDiv.hide();
         actionDiv.hide();
-
-        var editForm = '<form class="edit-comment" id="edit-c" method="" action="">'
-            + '<div class="form-group">' 
-            + '<textarea class="form-control" name="comment" rows="2">' + fixedComment + '</textarea>'
-            + '</div>'
-            + '<button type="button" class="btn btn-primary cancel" id="cancel-edit-c">Cancel</button>'
-            + '<button type="submit" class="btn btn-primary submit">Submit</button>'
-            + '</form>';
-        
-        $(editForm).appendTo(commentDiv);
+        editDiv.show();
     });
 
-    $('#cancel-edit-c').on('click', function(e) {
-        console.log($(this).attr('id'));
+    var cancelEdit = $('#cancel-comment');
+
+    $(document).on( 'click', '.cancel', function () {
+        currEditDiv.hide();
+        currCommentDiv.show();
+        currActionDiv.show();
     });
 });
