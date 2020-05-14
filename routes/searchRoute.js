6414ab3dc031d623f42780df3ac063009352e6f4
ludennis-dlsu <dennis_lu@dlsue.edu.ph>
2020-05-14 16:10:34 +0800

    Update searchRoute.js

--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -10,6 +10,6 @@ router.get('/popular/5star', controller.getSearchResult)
 router.get('/popular/4star', controller.getSearchResult)
 router.get('/popular/3star', controller.getSearchResult)
 router.get('/popular/2star', controller.getSearchResult)
-router.get('/popular/5star', controller.getSearchResult)
+router.get('/popular/1star', controller.getSearchResult)
 
 module.exports = router;
