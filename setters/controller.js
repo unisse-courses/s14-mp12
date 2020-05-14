1eb790dc055cb9e0984020cac8435cf048f99c97
ludennis-dlsu <dennis_lu@dlsue.edu.ph>
2020-05-14 22:48:39 +0800

    Fix Sort

--- setters/controller.js
+++ setters/controller.js
@@ -158,7 +158,8 @@ module.exports.getOtherUser = (req, res) => {
     if(req.path.includes('new')) {
         params.new = true;
 
-        UserAccount.findOne({username: username}).populate('recipePost').exec((err, user) => {
+        UserAccount.findOne({username: username}).populate({path: 'recipePost', options: { sort: {pfDate: -1} }}).exec((err, user) => {
+
             if(user) {
 
                 // Save to params for Layout
@@ -171,7 +172,7 @@ module.exports.getOtherUser = (req, res) => {
                 collection.find({ filename: filename }).toArray(function (err, docs) {
 
                     //Retrieving the chunks from the db
-                    collectionChunks.find({ files_id: docs[0]._id }).sort({ n: -1 }).toArray(function (err, chunks) {
+                    collectionChunks.find({ files_id: docs[0]._id }).sort({ n: 1 }).toArray(function (err, chunks) {
                         
                         //Append Chunks
                         let fileData = [];
